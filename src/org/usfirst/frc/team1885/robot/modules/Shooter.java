09d2e7126efb2999447105862de698e896913fba
Unknown <1natechong@gmail.com>
2017-09-14 17:13:56 -0400

    Completed setOuput() and fields/constructors of Shooter Class.

--- src/org/usfirst/frc/team1885/robot/modules/Shooter.java
+++ src/org/usfirst/frc/team1885/robot/modules/Shooter.java
@@ -1,5 +1,45 @@
 package org.usfirst.frc.team1885.robot.modules;
-
+import org.usfirst.frc.team1885.robot.sensors.*;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Talon;
 public class Shooter implements Module {
+	
+	private Talon angleMotor;
+	private PressureSensor pressureSensor;
+	private Potentiometer pot;
+	private Relay relay;
+	private double angle;
+	
+	public Shooter(PressureSensor pressureSensor, Potentiometer pot) {
+		this.pressureSensor = pressureSensor;
+		this.pot = pot;
+
+	}
+	@Override
+	public void init() {
+		this.angle = pot.getAngle();
+		angleMotor = new Talon(2);
+		
+	}
+
+	@Override
+	public boolean update() {
+		this.angle = pot.getAngle();
+		return false;
+	}
+	
+	public boolean dump() {
+		
+	}
+	
+	public void setOutput(double output) {
+		double limit1, limit2;
+		double threshold = 5;
+		if(Math.abs(angle - limit1)  < threshold || Math.abs(angle - limit2) < threshold)
+		{
+			angleMotor.set(0);//Won't turn motor when the angle is close to limit angles.
+		}
+		angleMotor.set(output);
+	}
 
 }
