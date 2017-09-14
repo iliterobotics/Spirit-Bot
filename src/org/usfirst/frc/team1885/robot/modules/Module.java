84c1a688f59ef134bd41853a65599b69d1d60c88
Stephen Welch <swordsnclaws@gmail.com>
2017-09-14 19:38:12 -0400

    Created all classes

---
+++ src/org/usfirst/frc/team1885/robot/modules/Module.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team1885.robot.modules;
+
+public interface Module {
+
+	void init();
+	boolean update();
+	
+}
---
+++ src/org/usfirst/frc/team1885/robot/sensors/Ultrasonic.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team1885.robot.sensors;
+
+public class Ultrasonic {
+
+}
