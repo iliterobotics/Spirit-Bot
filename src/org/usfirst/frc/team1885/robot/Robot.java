a26fd8914cf4fb5fd07b606d396347e37163820a
Stephen Welch <swordsnclaws@gmail.com>
2017-10-06 13:02:45 -0400

    Drivetrain DriverControl Robot implemented

--- src/org/usfirst/frc/team1885/robot/Robot.java
+++ src/org/usfirst/frc/team1885/robot/Robot.java
@@ -1,23 +1,41 @@
 
 package org.usfirst.frc.team1885.robot;
 
+import java.util.LinkedList;
+
+import org.usfirst.frc.team1885.robot.modules.*;
+import org.usfirst.frc.team1885.robot.sensors.PressureSensor;
+
 import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
-public class Robot extends SampleRobot{
-	     //This function is called once each time the robot enters autonomous mode.
-	     public void autonomous() {
-	          // Put code here
-	          Timer.delay(0.05);
-	     }
+public class Robot extends SampleRobot {
 
-	     // This function is called once each time the robot enters teleop mode.
-	     public void operatorControl() {
-	         while(isOperatorControl() && isEnabled()) {
-	               //Put code here
-	               //Timer.delay(0.05);
-	         }
-	     }
+	private LinkedList<Module> runningModules;
+	private DriveTrain drivetrain;
+	private Shooter shooter;
+	private Potentiometer angleSensor;
+	private PressureSensor pressureSensor;
 
-	
+	public void robotInit() {
+		runningModules = new LinkedList<>();
+		drivetrain = new DriveTrain();
+		angleSensor = new Potentiometer();
+		pressureSensor = new PressureSensor();
+		shooter = new Shooter(pressureSensor, angleSensor);
+	}
+
+	// This function is called once each time the robot enters teleop mode.
+	public void operatorControl() {
+		runningModules.add(drivetrain);
+		runningModules.add(shooter);
+		runningModules.add(angleSensor);
+		runningModules.add(pressureSensor);
+		while (isOperatorControl() && isEnabled()) {
+			for(Module m : runningModules) {
+				m.update();
+			}
+		}
+	}
+
 }
--- src/org/usfirst/frc/team1885/robot/modules/DriveTrain.java
+++ src/org/usfirst/frc/team1885/robot/modules/DriveTrain.java
@@ -1,5 +1,35 @@
 package org.usfirst.frc.team1885.robot.modules;
 
+import edu.wpi.first.wpilibj.Talon;
+
 public class DriveTrain implements Module {
 
+	public final int LEFT_PWM_ID = 0;
+	public final int RIGHT_PWM_ID = 1;
+	
+	Talon leftMotor, rightMotor;
+	double desiredLeft, desiredRight;
+	
+	public void init() {
+		leftMotor = new Talon(LEFT_PWM_ID);
+		rightMotor = new Talon(RIGHT_PWM_ID);
+		leftMotor.setBounds(2.0387, 1.539, 1.513, 1.487, .989);
+		rightMotor.setBounds(2.0387, 1.539, 1.513, 1.487, .989);
+	}
+	
+	public boolean update() {
+		updateSpeeds(desiredLeft, desiredRight);
+		return false;
+	}
+	
+	private void updateSpeeds(double left, double right) {
+		leftMotor.setSpeed(left);
+		rightMotor.setSpeed(right);
+	}
+	
+	public void setSpeeds(double left, double right) {
+		desiredLeft = left;
+		desiredRight = right;
+	}
+	
 }
