86305a466050f2f0e30df199aa2b2e3a67ecdf26
George <gealphonse22@gmail.com>
2017-10-12 16:58:29 -0400

    Updated Ultrasonic

--- src/org/usfirst/frc/team1885/robot/sensors/Ultrasonic.java
+++ src/org/usfirst/frc/team1885/robot/sensors/Ultrasonic.java
@@ -1,6 +1,31 @@
 package org.usfirst.frc.team1885.robot.sensors;
+import edu.wpi.first.wpilibj.AnalogInput;
 
-public class Ultrasonic 
+public class Ultrasonic
 {
+	private static final double SCALE_FACTOR = 0.2; //needs to be tested
+	private AnalogInput analogInput;
+	private static final int PORT;
+	private double voltage;
+	
+	public Ultrasonic(int port)
+	{
+		
+		analogInput = new AnalogInput(PORT);
+	}
+	
+	
+	private boolean update()
+	{
+		voltage = analogInput.getVoltage();
+		return true;
+	}
+	
+
+	public double getDistance()
+	{
+		//return voltage * SCALE_FACTOR;
+		return 0.0;
+	}
 	
 }
