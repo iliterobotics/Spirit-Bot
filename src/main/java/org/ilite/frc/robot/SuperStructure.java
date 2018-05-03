e1dd97c9f2152ea485825b309b5b675cd6b1a8be
SilentWanderer <stephenwelchva@gmail.com>
2018-05-03 16:31:57 -0400

    Add SuperStructure class

---
+++ src/main/java/org/ilite/frc/robot/SuperStructure.java
@@ -0,0 +1,21 @@
+package org.ilite.frc.robot;
+
+/**
+ * Wraps any logic needed to control the robot. In 2018, this logic was part of the DriverControl class.
+ * This class should NOT contain ANY logic that can be put into a specific subsystem.
+ */
+public class SuperStructure {
+
+    public void dump() {
+
+    }
+
+    public void fire() {
+
+    }
+
+    public void moveShooter(double power) {
+
+    }
+
+}
