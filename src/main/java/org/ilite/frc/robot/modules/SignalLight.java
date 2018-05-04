71439dcf19f837dbd92d8cab007602baff79c60b
SilentWanderer <stephenwelchva@gmail.com>
2018-05-04 07:04:55 -0400

    Clean up SignalLight

--- src/main/java/org/ilite/frc/robot/modules/SignalLight.java
+++ src/main/java/org/ilite/frc/robot/modules/SignalLight.java
@@ -2,21 +2,26 @@ package org.ilite.frc.robot.modules;
 
 import edu.wpi.first.wpilibj.Relay;
 import org.ilite.frc.robot.Constants;
+import org.ilite.frc.robot.util.TimedBoolean;
 
 public class SignalLight implements IModule {
 
     private Relay signalLight;
     private SignalState currentState;
-    private long blinkStartTime;
+    private TimedBoolean timer; // Returns true when time to transition from off to on or on to off
     private boolean isBlinkingOn;
 
+    /**
+     * Allows us to define values for on/off in one place in case we find out that, for example, "on" is Value.kForward.
+     * Also defines how long we blink on/off for.
+     */
     public enum SignalState {
-        OFF(Relay.Value.kOff, -1.0), READY_TO_FIRE(Relay.Value.kOn, 0.3);
+        OFF(Relay.Value.kOff, -1), READY_TO_FIRE(Relay.Value.kOn, 300);
 
         Relay.Value relayValue;
-        double blinkTime;
+        long blinkTime;
 
-        SignalState(Relay.Value relayValue, double delay) {
+        SignalState(Relay.Value relayValue, long delay) {
             this.relayValue = relayValue;
             this.blinkTime = delay;
         }
@@ -24,24 +29,23 @@ public class SignalLight implements IModule {
 
     public SignalLight() {
         signalLight = new Relay(Constants.LED_RELAY);
+        timer = new TimedBoolean(0);
     }
 
     @Override
     public void init() {
         currentState = SignalState.OFF;
+        isBlinkingOn = false;
+        timer.reset(0);
     }
 
     @Override
     public boolean update() {
         switch(currentState) {
             case READY_TO_FIRE:
-                if(!isBlinkingOn && System.currentTimeMillis() - blinkStartTime >= currentState.blinkTime) {
-                    isBlinkingOn = true;
-                    blinkStartTime = System.currentTimeMillis();
-                    signalLight.set(currentState.relayValue);
-                } else {
-                    isBlinkingOn = false;
-                    signalLight.set(SignalState.OFF.relayValue);
+                if(timer.get()) { // If timer is done
+                    timer.reset(currentState.blinkTime); // Reset the timer
+                    toggleLight(currentState, isBlinkingOn); // Toggle light on/off
                 }
                 break;
             case OFF:
@@ -55,6 +59,19 @@ public class SignalLight implements IModule {
 
     public void setState(SignalState desiredState) {
         currentState = desiredState;
+    }
+
+    /**
+     * Toggles light between an "on" state and the "off" state
+     * @param currentState The state describing what "on" is
+     * @param isLightOn Whether the physical light is on or not.
+     */
+    private void toggleLight(SignalState currentState, boolean isLightOn) {
+        if(isLightOn) { // Toggle light on/off
+            signalLight.set(SignalState.OFF.relayValue);
+        } else {
+            signalLight.set(currentState.relayValue);
+        }
     }
 
 }
---
+++ src/main/java/org/ilite/frc/robot/util/TimedBoolean.java
@@ -0,0 +1,23 @@
+package org.ilite.frc.robot.util;
+
+public class TimedBoolean {
+
+    private boolean value = false;
+    private long triggerTime; // In MS
+
+    public TimedBoolean(long delay) {
+        this.reset(delay);
+    }
+
+    public boolean get() {
+        if(System.currentTimeMillis() >= triggerTime) {
+            return true;
+        }
+        return false;
+    }
+
+    public void reset(long delay) {
+        this.triggerTime = System.currentTimeMillis() + delay;
+    }
+
+}
